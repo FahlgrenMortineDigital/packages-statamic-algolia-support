ae14821375f6fcbbc2446829a8c8d8ac03530457
Andrew Clinton <ddrew.clinton@gmail.com>
2025-01-30 15:49:30 -0500

    readme and composer update

--- config/algolia-support.php
+++ config/algolia-support.php
@@ -1,7 +1,21 @@
 <?php
 
 return [
+    /** ---------------------------------------------------------------
+     *  Configure the disk where index exports will be stored
+     * ----------------------------------------------------------------
+     */
     'disk' => env('ALGOLIA_SUPPORT_DISK', 'algolia-indexes'),
+
+    /** ----------------------------------------------------------------
+     * Configure the API URI for the Algolia ingest service to connect.
+     * ----------------------------------------------------------------
+     */
     'api_uri' => env('ALGOLIA_SUPPORT_API_URI', '/algolia/indexes'),
+
+    /** ----------------------------------------------------------------
+     * The memory limit for the Algolia index export builder command.
+     * ----------------------------------------------------------------
+     */
     'memory_limit' => '2028M'
 ];
