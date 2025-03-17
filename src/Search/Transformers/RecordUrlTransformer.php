7b9442f88af106407c24d3ee51ccfbcfdfed9683
Andrew Clinton <ddrew.clinton@gmail.com>
2025-03-17 14:25:21 -0400

    fix bad if logic

--- src/Search/Transformers/RecordUrlTransformer.php
+++ src/Search/Transformers/RecordUrlTransformer.php
@@ -6,10 +6,6 @@ class RecordUrlTransformer
 {
     public function handle($value, $field, $searchable): ?string
     {
-        if (empty($value)) {
-            return null;
-        }
-
         return match (true) {
             $searchable instanceof \Statamic\Entries\Entry => $searchable->url(),
             $searchable instanceof \Statamic\Assets\Asset => $searchable->url(),
